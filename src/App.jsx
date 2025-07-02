19194c6a86e5ec7763725acb16972be1c96f7e65
Daniela Conde <daniela_conde2@hotmail.com>
2025-07-02 11:37:00 -0600

    added LoadScript and routes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { LoadScript } from "@react-google-maps/api";
 import LoginPage from "./pages/login/LoginPage";
 import "./App.scss";
+
+const googleMapsKey = import.meta.env.VITE_APP_GOOGLE_API_KEY;
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <LoadScript googleMapsApiKey={googleMapsKey}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </LoadScript>
     </>
   );
 }
